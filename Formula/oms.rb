b165ae8caad7923eb34873a5fea9beccdf5ab19f
christian.schulz.phone@gmail.com <christian.schulz.phone@gmail.com>
2026-03-11 11:14:14 +0000

    Add OMS (OnlineMultiSection) formula

---
+++ Formula/oms.rb
@@ -0,0 +1,44 @@
+class Oms < Formula
+  desc "OMS - Streaming Process Mapping and Hierarchical Graph Partitioning"
+  homepage "https://github.com/KaHIP/OnlineMultiSection"
+  url "https://github.com/KaHIP/OnlineMultiSection/archive/refs/tags/v1.0.tar.gz"
+  sha256 "c7bb835d7bedd9a1d83aff8bc0f872b22ce22fd1d1dda0f61ad3b43113a70fb2"
+  license "MIT"
+  head "https://github.com/KaHIP/OnlineMultiSection.git", branch: "master"
+
+  depends_on "cmake" => :build
+  depends_on "gcc" => :build
+  depends_on "open-mpi"
+
+  def install
+    gcc = Formula["gcc"]
+    gcc_version = gcc.version.major
+
+    cmake_args = std_cmake_args.reject { |a| a.start_with?("-DCMAKE_PROJECT_TOP_LEVEL_INCLUDES=") }
+
+    system "cmake", "-B", "build",
+                    "-DCMAKE_BUILD_TYPE=Release",
+                    "-DCMAKE_POLICY_VERSION_MINIMUM=3.5",
+                    "-DCMAKE_C_COMPILER=#{gcc.opt_bin}/gcc-#{gcc_version}",
+                    "-DCMAKE_CXX_COMPILER=#{gcc.opt_bin}/g++-#{gcc_version}",
+                    "-DCMAKE_C_FLAGS=-w",
+                    "-DCMAKE_CXX_FLAGS=-w",
+                    "-DNONATIVEOPTIMIZATIONS=ON",
+                    *cmake_args
+    system "cmake", "--build", "build", "-j#{ENV.make_jobs}"
+
+    bin.install "build/streammultisection"
+  end
+
+  test do
+    (testpath/"test.graph").write <<~EOS
+      4 5
+      2 3
+      1 3 4
+      1 2 4
+      2 3
+    EOS
+    output = shell_output("#{bin}/streammultisection #{testpath}/test.graph --k=2 2>&1")
+    assert_match(/cut|partition|block/, output)
+  end
+end
